cc58f3aad528d44d6225280ce933ff27f91a90b4
dharshinisekar08 <24bcs210@kgcas.com>
2026-01-03 15:05:59 +0530

    program1

--- App.jsx
+++ App.jsx
@@ -1,54 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react'
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [text, setText] = useState("");
-
-  const addTask = () => {
-    if (text.trim() === "") return;
-
-    setTasks([...tasks, { name: text, completed: false }]);
-    setText("");
-  };
-
-  const deleteTask = (index) => {
-    const newTasks = tasks.filter((_, i) => i !== index);
-    setTasks(newTasks);
-  };
-
-  const toggleTask = (index) => {
-    const newTasks = [...tasks];
-    newTasks[index].completed = !newTasks[index].completed;
-    setTasks(newTasks);
-  };
-
   return (
     <div>
-      <h1>Todo List</h1>
-
-      <input
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Enter task"
-      />
-
-      <button onClick={addTask}>Add Task</button>
-
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            <input
-              type="checkbox"
-              checked={task.completed}
-              onChange={() => toggleTask(index)}
-            />
-            <span>{task.name}</span>
-            <button onClick={() => deleteTask(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      <p class="bg-amber-100">Lorem ipsum dolor sit amet.</p>
+      <p class="bg-amber-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, similique?</p>
+      <p class="bg-amber-200">Lorem ipsum dolor sit amet consectetur</p>
+      <p class="bg-amber-400">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+      <p class="bg-amber-600">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
